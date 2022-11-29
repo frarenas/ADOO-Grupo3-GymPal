f99b3b286c7f2f701bcf738e20f2f43df468ba15
Federico <feferreyra98@gmail.com>
2022-11-29 19:36:39 -0300

    Carga de Ejercicios al iniciar la aplicacion

--- src/ar/edu/uade/ui/Main.java
+++ src/ar/edu/uade/ui/Main.java
@@ -1,8 +1,13 @@
 package ar.edu.uade.ui;
 
+import ar.edu.uade.controller.EjercicioController;
 import ar.edu.uade.controller.SocioController;
 import ar.edu.uade.enums.SexoBiologico;
+import ar.edu.uade.model.ejercicio.Ejercicio;
+import ar.edu.uade.model.ejercicio.TotalEjercicios;
 import ar.edu.uade.model.socio.Socio;
+
+import java.util.List;
 
 public class Main {
 
@@ -16,8 +21,16 @@ public class Main {
 		Socio socio2 = new Socio(1L, "Federico", "12341234", 30, SexoBiologico.MASCULINO, "federico@example.com", 1.8, 80);
 		Socio socio3 = new Socio(2L, "Francisco", "12344321", 35, SexoBiologico.MASCULINO, "francisco@example.com", 1.9, 90);
 
+		//Mock de Objetos
 		SocioController.socioDB.put(socio1.getId(), socio1);
 		SocioController.socioDB.put(socio2.getId(), socio2);
 		SocioController.socioDB.put(socio3.getId(), socio3);
+
+		List<Ejercicio> ejercicioRepository = new TotalEjercicios().getTotalEjercicios();
+		for(Ejercicio e:ejercicioRepository){
+			EjercicioController.ejercicioDB.put(e.getId(),e);
+		}
+
+
 	}
 }
