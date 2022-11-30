046e364a250115c7c63c5cd3d80f86b6865c9d56
Ignacio Cóceres <67297232+IgnacioCoceres@users.noreply.github.com>
2022-11-30 00:52:19 -0300

    Creacion de trofeos

--- src/ar/edu/uade/model/trofeo/TrofeoConstancia.java
+++ src/ar/edu/uade/model/trofeo/TrofeoConstancia.java
@@ -1,14 +1,24 @@
 package ar.edu.uade.model.trofeo;
 
+import ar.edu.uade.abstractions.IAdapterNotificacionPush;
+import ar.edu.uade.model.AdapterFirebase;
+import ar.edu.uade.model.Notificacion;
 import ar.edu.uade.model.Rutina;
 import ar.edu.uade.model.socio.Socio;
 
-public class TrofeoConstancia extends Trofeo{
+public class TrofeoConstancia extends Trofeo {
 
     private Rutina rutinaObservado;
 
     @Override
     public void generarTrofeo(Socio socio) {
+        IAdapterNotificacionPush not = new AdapterFirebase();
+        Notificacion mensaje = new Notificacion();
+        mensaje.setMensaje("Has ganado el Trofeo Constancia");
+        not.enviar(mensaje);
+        socio.addTrofeo(this);
 
     }
+
+
 }
--- src/ar/edu/uade/model/trofeo/TrofeoDedicacion.java
+++ src/ar/edu/uade/model/trofeo/TrofeoDedicacion.java
@@ -1,5 +1,8 @@
 package ar.edu.uade.model.trofeo;
 
+import ar.edu.uade.abstractions.IAdapterNotificacionPush;
+import ar.edu.uade.model.AdapterFirebase;
+import ar.edu.uade.model.Notificacion;
 import ar.edu.uade.model.objetivo.Objetivo;
 import ar.edu.uade.model.socio.Socio;
 
@@ -10,6 +13,12 @@ public class TrofeoDedicacion extends Trofeo {
 
     @Override
     public void generarTrofeo(Socio socio) {
-
+        IAdapterNotificacionPush not = new AdapterFirebase();
+        Notificacion mensaje = new Notificacion();
+        mensaje.setMensaje("Has ganado el Trofeo Dedicación");
+        not.enviar(mensaje);
+        socio.addTrofeo(this);
     }
+
+
 }
