24b3678c9abccf1be3d5bda67880021d68dd7ce7
Federico <feferreyra98@gmail.com>
2022-11-29 21:38:38 -0300

    Implementado el metodo boolean rutinaCompleta() Des-comentado el atributo duracionSemanas de la Clase Rutina

--- src/ar/edu/uade/model/Rutina.java
+++ src/ar/edu/uade/model/Rutina.java
@@ -7,30 +7,32 @@ import java.util.List;
 
 public class Rutina {
 
-	//private final int duracionSemanas = 4;
-	private List<Entrenamiento> entrenamientos = new LinkedList<Entrenamiento>();
+	private int duracionSemanas = 4;
+	private List<Entrenamiento> entrenamientos;
 
 	public Rutina(List<Entrenamiento> entrenamientos) {
 		this.entrenamientos = entrenamientos;
 	}
 
-	public Rutina crearRutina(ExigenciaMuscular nivelExigencia, int nivelAerobicoMin, int nivelAerobicoMax){
+	public void crearRutina(ExigenciaMuscular nivelExigencia, int nivelAerobicoMin, int nivelAerobicoMax){
 
 		for(int i =0; i<5; i++){
 			Entrenamiento entrenamiento = new Entrenamiento();
 			entrenamiento.crearEntrenamiento(nivelExigencia, nivelAerobicoMin, nivelAerobicoMax);
 			this.entrenamientos.add(entrenamiento);
 		}
-		return this;
-
 	}
 	public void reforzar(){
 		Entrenamiento entrenamientoReforzado = this.entrenamientos.get(entrenamientos.size()-1);
 		entrenamientoReforzado.reforzar();
 		entrenamientos.set(entrenamientos.size() - 1, entrenamientoReforzado);
 	}
+	public void simularSemana(){
+		if (duracionSemanas>0)
+			duracionSemanas--;
+	}
 	public boolean rutinaCompleta(){
-		throw new UnsupportedOperationException();
+		return duracionSemanas == 0;
 	}
 
 }
