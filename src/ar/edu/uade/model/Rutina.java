ad9461b126960e4291b9fea179278269548635d2
Ignacio Cóceres <67297232+IgnacioCoceres@users.noreply.github.com>
2022-11-29 22:29:46 -0300

    Corrigiendo Rutina

--- src/ar/edu/uade/model/Rutina.java
+++ src/ar/edu/uade/model/Rutina.java
@@ -7,32 +7,30 @@ import java.util.List;
 
 public class Rutina {
 
-	private int duracionSemanas = 4;
-	private List<Entrenamiento> entrenamientos;
+	//private final int duracionSemanas = 4;
+	private List<Entrenamiento> entrenamientos = new LinkedList<Entrenamiento>();
 
 	public Rutina(List<Entrenamiento> entrenamientos) {
 		this.entrenamientos = entrenamientos;
 	}
 
-	public void crearRutina(ExigenciaMuscular nivelExigencia, int nivelAerobicoMin, int nivelAerobicoMax){
+	public Rutina crearRutina(ExigenciaMuscular nivelExigencia, int nivelAerobicoMin, int nivelAerobicoMax, String duracionEntrenamiento){
 
 		for(int i =0; i<5; i++){
 			Entrenamiento entrenamiento = new Entrenamiento();
-			entrenamiento.crearEntrenamiento(nivelExigencia, nivelAerobicoMin, nivelAerobicoMax);
+			entrenamiento.crearEntrenamiento(nivelExigencia, nivelAerobicoMin, nivelAerobicoMax, duracionEntrenamiento);
 			this.entrenamientos.add(entrenamiento);
 		}
+		return this;
+
 	}
 	public void reforzar(){
 		Entrenamiento entrenamientoReforzado = this.entrenamientos.get(entrenamientos.size()-1);
 		entrenamientoReforzado.reforzar();
 		entrenamientos.set(entrenamientos.size() - 1, entrenamientoReforzado);
 	}
-	public void simularSemana(){
-		if (duracionSemanas>0)
-			duracionSemanas--;
-	}
 	public boolean rutinaCompleta(){
-		return duracionSemanas == 0;
+		throw new UnsupportedOperationException();
 	}
 
 }
